06448f3d55770aa9bcd9d6c0ac1dcbbfa853ec58
kevinlimadev <113479682+kevinlimadev@users.noreply.github.com>
2022-10-02 19:27:53 -0300

    Primeira Versão

---
+++ script.js
@@ -0,0 +1,11 @@
+$('button').on('mouseover', function() {
+    $(this).animate({
+      'top': Math.random() * document.
+                getElementById('content').
+                clientHeight + "px",
+      'left': Math.random() * document.
+                getElementById('content').
+                clientWidth + "px"
+    }, "medium");
+  })
+
